e6c3f4ae1fd25b1db5d8547ec4a636fc83f139e7
PavloZalevsky <pavlo.zalevsky@gmail.com>
2016-10-02 21:13:44 +0300

    ADD: Level Config

---
+++ Assets/scripts/LevelConfig.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using System.IO;
+[InitializeOnLoad]
+#endif
+
+public class LevelConfig : ScriptableObject
+{
+
+    #region Edit + Editor + Singleton
+
+#if UNITY_EDITOR
+    [MenuItem("Level/Level Config")]
+    public static void Edit()
+    {
+        Selection.activeObject = LevelConfig.instance;
+    }
+#endif
+
+    private const string ISNSettingsAssetName = "LevelConfigSettings";
+    private const string ISNSettingsPath = "Resources";
+    private const string ISNSettingsAssetExtension = ".asset";
+
+    private static LevelConfig _instance = null;
+
+    public static LevelConfig instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Resources.Load(ISNSettingsAssetName) as LevelConfig;
+
+                if (_instance == null)
+                {
+                    _instance = CreateInstance<LevelConfig>();
+#if UNITY_EDITOR
+                    string fullPath = Path.Combine(Path.Combine("Assets", ISNSettingsPath),
+                                                   ISNSettingsAssetName + ISNSettingsAssetExtension
+                                                   );
+                    AssetDatabase.CreateAsset(_instance, fullPath);
+#endif
+                }
+            }
+            return _instance;
+        }
+    }
+
+    #endregion
+
+    public List<Level> levels = new List<Level>();
+
+
+
+
+}
+
+
+[System.Serializable]
+public class Level
+{
+    public int Time;
+    public int CountEnemy;
+    public int MaxSpeedEnemy;
+    public int MinSpeedEnemy;
+}
